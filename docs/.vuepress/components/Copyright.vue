c013abb0059f0fa95f7be06dda2f8274c5d58417
nightinwinter <70999718+nightinwinter@users.noreply.github.com>
2023-08-08 19:24:46 +0800

    Signed-off-by: nightinwinter <70999718+nightinwinter@users.noreply.github.com>

---
+++ docs/.vuepress/components/Copyright.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div v-html="html"/>
+<p>
+<br>
+——————————
+作者：zhāngpéngjūn
+链接：<a :href="location">{{location}}</a>
+来源：znote
+版权声明：著作权归原作者所有，商业转载请联系作者获得授权，非商业转载请注明出处。
+——————————
+</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['html'],
+
+        created() {
+            this.location = window.location
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ docs/.vuepress/theme/components/LoadingPage.vue
@@ -0,0 +1,87 @@
+<template>
+  <div id="loader-wrapper">
+    <h3 class="title" v-if="$site">{{$site.title}}</h3>
+    <p class="description" v-if="$site">{{$site.description}}</p>
+    <div class="loading">
+      <span
+        :style="{
+          animationDuration:
+            duration.indexOf('s') === -1 ? duration + 's' : duration,
+          animationDelay: (parseInt(duration) / text.length) * index + 's',
+        }"
+        v-for="(char, index) in text"
+        :key="index"
+      >
+        {{ char }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: "Loading...",
+    },
+    duration: {
+      type: String,
+      default: "2s",
+    },
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+#loader-wrapper
+  height: 100%;
+  width: 100%;
+  background var(--background-color)
+  .loading{
+    width 200px
+    margin 5rem auto
+    display: flex;
+    text-align center
+    justify-content: space-between;
+    background #23#0aafe6
+  }
+  .loading span{
+    display: block;
+    width: 20px;
+    height: 22px;
+    border-radius: 4px;
+    animation: ease infinite move;
+  }
+  @keyframes move {
+    0% {
+      margin-top: -10px;
+      border-bottom: 1px solid;
+    }
+    50% {
+      margin-top: 10px;
+      border-bottom: none;
+    }
+    100% {
+      margin-top: -10px;
+    }
+  }
+  .title
+    margin 15% auto 1rem
+    text-align center
+    color $textColor
+    color var(--text-color)
+    font-size 40px
+    box-sizing: border-box;
+    padding: 0 10px;
+    text-shadow 0 2px 10px rgba(0,0,0,0.2)
+  .description
+    margin auto
+    text-align center
+    color $textColor
+    color var(--text-color)
+    font-size 25px
+    box-sizing: border-box;
+    padding: 0 10px;
+    text-shadow 0 2px 10px rgba(0,0,0,0.2);
+</style>
